ce2dfde9d3a237e79564e30bb02c7fa64702ec69
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2020-05-18 00:00:36 +0900

    Make ProjectPath not required and set default value "."

--- models/options.go
+++ models/options.go
@@ -1,7 +1,7 @@
 package models
 
 type Options struct {
-	ProjectPath string `short:"p" long:"project" description:"Path to unity project" required:"yes"`
+	ProjectPath string `short:"p" long:"project" description:"Path to unity project" default:"."`
 	Output      string `short:"o" long:"output" description:"Output file"`
 	RaiseError  bool   `short:"e" long:"raise-error" description:"Exit as error if even one is listed"`
 }
