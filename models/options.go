fd6e2b7bdd167295e547843e826711ef14030d07
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2020-05-19 04:28:36 +0900

    Add option -t, --template

--- models/options.go
+++ models/options.go
@@ -1,7 +1,8 @@
 package models
 
 type Options struct {
-	ProjectPath string `short:"p" long:"project" description:"Path to unity project" default:"."`
-	Output      string `short:"o" long:"output" description:"Output file"`
-	RaiseError  bool   `short:"e" long:"raise-error" description:"Exit as error if even one is listed"`
+	ProjectPath  string `short:"p" long:"project" description:"Path to unity project" default:"."`
+	Output       string `short:"o" long:"output" description:"Output file"`
+	RaiseError   bool   `short:"e" long:"raise-error" description:"Exit as error if even one is listed"`
+	TemplatePath string `short:"t" long:"template" description:"Path to template file used to rendering reports"`
 }
